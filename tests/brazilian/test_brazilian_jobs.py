1d333f2cfb93cb013981872632d5434d2cf7080b
Igor Campos Moreira <igorcampos200@gmail.com>
2022-07-28 13:46:52 -0300

    Requisito 11?

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,13 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    coverted_jobs = read_brazilian_file('tests/mocks/brazilian_jobs.csv')
+    english_keys = {
+        "title": "str",
+        "salary": "str",
+        "type": 'str'
+    }
+
+    for job in coverted_jobs:
+        assert job.keys() == english_keys.keys()
