0726c6676fd5213df368af352125546350341338
Igor Campos Moreira <igorcampos200@gmail.com>
2022-07-28 15:48:07 -0300

    requisito 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -2,11 +2,11 @@ from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    coverted_jobs = read_brazilian_file('tests/mocks/brazilian_jobs.csv')
+    coverted_jobs = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
     english_keys = {
         "title": "str",
         "salary": "str",
-        "type": 'str'
+        "type": "str"
     }
 
     for job in coverted_jobs:
